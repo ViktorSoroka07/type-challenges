5930626d287803b2b394353f412eead65952515c
Viktor Soroka <viktor_soroka@epam.com>
2025-04-26 19:46:34 +0300

    Implement `Trim`

--- questions/00108-medium-trim/template.ts
+++ questions/00108-medium-trim/template.ts
@@ -1,1 +1,5 @@
-type Trim<S extends string> = any
+type WhiteSpace = ' ' | '\n' | '\t'
+type TrimStart<S extends string> = S extends `${WhiteSpace}${infer Rest}` ? TrimStart<Rest> : S
+type TrimEnd<S extends string> = S extends `${infer Rest}${WhiteSpace}` ? TrimEnd<Rest> : S
+
+type Trim<S extends string> = TrimStart<TrimEnd<S>>
