3cab1b0bfd9273b3fbf06da02edb80228fe581a7
Viktor Soroka <viktor_soroka@epam.com>
2025-04-26 11:45:57 +0300

    Implement `TupleToObject`

--- questions/00011-easy-tuple-to-object/test-cases.ts
+++ questions/00011-easy-tuple-to-object/test-cases.ts
@@ -7,6 +7,8 @@ const sym2 = Symbol(2)
 const tupleSymbol = [sym1, sym2] as const
 const tupleMix = [1, '2', 3, '4', sym1] as const
 
+type B = TupleToObject<typeof tuple>
+
 type cases = [
   Expect<Equal<TupleToObject<typeof tuple>, { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y' }>>,
   Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1, 2: 2, 3: 3, 4: 4 }>>,
